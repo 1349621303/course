f63acd457868516a79639250ba5ec15761d542ab
lgx <1349621303@qq.com>
2020-09-01 22:31:45 +0800

    对 gateway 添加 system 路由转发

--- system/src/main/java/com/course/system/controller/TestController.java
+++ system/src/main/java/com/course/system/controller/TestController.java
@@ -3,6 +3,8 @@ package com.course.system.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// 整个类下面的 url 都会拼接一个 system 的前缀路径
+//@RequestMapping("/system")
 @RestController
 public class TestController {
 
