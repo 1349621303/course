927bb3dae2c30b5dbc29d1c7dfba8fbb005fc0c4
lgx <1349621303@qq.com>
2020-09-02 21:59:20 +0800

    新建公共模块 server

---
+++ server/src/main/java/com/course/server/domain/Test.java
@@ -0,0 +1,23 @@
+package com.course.server.domain;
+
+public class Test {
+    private String id;
+    // command + D 表示快速复制一行
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
