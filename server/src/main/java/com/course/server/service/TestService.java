7c9c768a77ff9906a554ce1eeb11f57a24b8e956
lgx <1349621303@qq.com>
2020-09-03 13:49:17 +0800

    增加了 mybatis-plus 的 Example 示例，可以快速对单表进行CURD的操作

--- server/src/main/java/com/course/server/service/TestService.java
+++ server/src/main/java/com/course/server/service/TestService.java
@@ -1,6 +1,7 @@
 package com.course.server.service;
 
 import com.course.server.domain.Test;
+import com.course.server.domain.TestExample;
 import com.course.server.mapper.TestMapper;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,14 @@ public class TestService {
     private TestMapper testMapper;
 
     public List<Test> list() {
-        return testMapper.list();
+        TestExample testExample = new TestExample();
+
+        // 相当于 where 查询语句
+        testExample.createCriteria().andIdEqualTo("1");
+        // 这种方式是降序排序
+        // testExample.setOrderByClause("id desc");
+
+
+        return testMapper.selectByExample(testExample);
     }
 }
